274499230d581be16c638c8061cf989fdb6710a1
samuel-18o <samuel.18.09.05@gmail.com>
2026-01-27 21:17:07 -0500

    Crear app.js inicial y corregir su ruta en index.html

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// js/app.js
+
+import { initRouter } from './router/router.js';
+
+/**
+ * Punto de entrada de la aplicación
+ * Inicializa el router SPA
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  initRouter();
+});
